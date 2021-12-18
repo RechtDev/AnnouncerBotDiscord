ee0fc41b7493fc40d9de5d718e639a146ea09d07
Domonic Francis <rechtproductions@gmail.com>
2021-12-18 15:45:53 -0500

    Added the setup requirments for the bot to connect to discord

---
+++ AnnouncerBot/AnnouncerBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace AnnouncerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync()
+        {
+            string secretToken = ConfigurationManager.AppSettings.Get("BotToken");
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = secretToken,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+    }
+}
